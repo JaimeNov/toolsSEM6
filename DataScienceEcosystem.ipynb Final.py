6cdd43e0332679406d7cbca375441fd7edb37151
JaimeNov <135549817+JaimeNov@users.noreply.github.com>
2023-06-11 10:49:10 -0500

    Add files via upload

---
+++ DataScienceEcosystem.ipynb Final.py
@@ -0,0 +1,58 @@
+# %% [markdown]
+# # Data Sciencie Tools and Ecosystem
+
+# %% [markdown]
+# <p> In this notebook, Data Sciencie Tools and Ecosystem are summarized </p>
+
+# %% [markdown]
+# <P> Some of the popular languages that Data Scientists use are: </P> 
+# <ul> ordered list </ul>
+# <li>Python</li>
+# <li>CSS</li>
+# <li>Java</li>
+
+# %% [markdown]
+# <p>Some of the commonly used libraries used by Data Scientists include:</p>
+# <ul>ordered list </ul>
+# <li>Pandas</li>
+# <li>Numpy</li>
+# <li>Scipy</li>
+# <li>TensorFlow</li>
+
+# %% [markdown]
+# <p>Data Sciencie Tools</p>
+# <li>Anaconda</li>
+# <li>Scikit-learn</li>
+# <li>Spark</li>
+
+# %% [markdown]
+# <h3>Below are a few examples of evaluating arithmetic expressions in Python</h3>
+# 
+
+# %%
+print ((3*4) + 5)
+
+# %% [markdown]
+# <h4> This a simple arithmetic expression to mutiply then add integers</h4>
+
+# %%
+print (200/60)
+
+# %% [markdown]
+# <h4> his will convert 200 minutes to hours by diving by 60</h4>
+
+# %% [markdown]
+# <h1>Objectives:</h1>
+# <li>Create a Jupyter Notebook with markdown and code cells</li>
+# <li>List examples of languages, libraries and tools used in Data Science</li>
+# <li>Share your Jupyter Notebook publicly on GitHub</li>
+# <li>Evaluate notebooks submitted by your peers using the provided rubric</li>
+# <li>Demonstrate proficiency in Data Science toolkit knowledge</li>
+# 
+
+# %% [markdown]
+# <h2>Author</h2>
+# Jaime Andre Novoa
+# 
+
+
